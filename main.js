0c98fd4d8b8d0843f3df029962f4413babe00a1a
VAIDANT <72136359+VAIDANT@users.noreply.github.com>
2021-07-24 14:01:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/6-vkpic6K/model.json', modelLoaded);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML = result[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);
+    }
+}
